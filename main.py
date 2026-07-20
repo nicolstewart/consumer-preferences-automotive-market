c769754d1446460bbc678776ec83bd3c68627e70
nicolstewart <nicolstewart.business@gmail.com>
2026-07-20 14:18:29 +0200

    Data cleaning

--- main.py
+++ main.py
@@ -53,12 +53,65 @@ print(df.isnull().sum())
 # Looking for duplicate Records
 print(df.duplicated().sum())
 
+# Create working dataset
+df_clean = df.copy()
+
+#============================================================================================================================================================
+# Check numerical ranges
+print("-" * 60)
+print(df_clean.describe())
+
+#============================================================================================================================================================
+# Feature Engineering
+#============================================================================================================================================================
+# Vehicle age
+
+CURRENT_YEAR = 2026
+df_clean["Vehicle_Age"] = CURRENT_YEAR - df_clean["Model_Year"]
+print("-" * 60)
+print("Vehicle Age:")
+print(df_clean[["Model_Year", "Vehicle_Age"]].head())
+
+# Price per horsepower
+df_clean["Price_per_bhp"] = (
+    df_clean["Price_USD"] /
+    df_clean["Max_Power_bhp"]
+)
+print("Price per Horsepower:")
+print(df_clean[["Price_USD", "Max_Power_bhp"]].head())
+
+# Classifying into two segments (premium and mass market)
+premium_brands = [
+    "Audi",
+    "BMW",
+    "Mercedes"
+]
+
+df_clean["Brand_Segment"] = df_clean["Brand"].apply(
+    lambda brand: "Premium"
+    if brand in premium_brands
+    else "Mass Market"
+)
+
+# Grouping Vehicles based on their mileage
+df_clean["Mileage_Category"] = pd.cut(
+    df_clean["Kilometers_Driven"],
+    bins=[0, 50000, 100000, 150000, 200000],
+    labels=[
+        "Low",
+        "Moderate",
+        "High",
+        "Very High"
+    ]
+)
+
+# Inspecting new dataset
+print(df_clean.head())
+print(df_clean.info())
 
 
-
-
-
-
-
-
-
+# Saving clean dataset
+df_clean.to_csv(
+    "data/used_car_dataset_clean.csv",
+    index=False
+)
